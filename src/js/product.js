36e7d3b4a5fb287b0a514fe8c47f1c567a62bd38
Giorubio27 <Giorubio27@gmail.com>
2026-03-11 15:31:25 -0600

    done

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,23 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart");
+
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+  cart.push(product);
+
+  setLocalStorage("so-cart", cart);
+
+  console.log(`${product.Name} added to your cart`);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
