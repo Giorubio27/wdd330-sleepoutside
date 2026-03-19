b0e4ad24eb642bad617d8484434910c864f8295c
Giorubio27 <Giorubio27@gmail.com>
2026-03-19 07:53:35 -0600

    added ind

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,34 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+    <a href="product_pages/?product=${product.Id}">
+        <img src="${product.Image}" alt="${product.NameWithoutBrand}">
+        <h2 class="card_name">${product.NameWithoutBrand}</h2>
+        <h3 class="card_brand">${product.Brand.Name}</h3>
+        <p class="product-card_price">${product.FinalPrice}</p>`;
+}
+
+export default class ProductList {
+
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+        
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
+
+}
+
+
+    
