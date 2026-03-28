53554e94d661aa4ceaa1989cd83f13910e9918ff
Giorubio27 <Giorubio27@gmail.com>
2026-03-28 08:59:04 -0600

    commiting the category changes in productlisting

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,11 +1,17 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 
 
 loadHeaderFooter();
 
-const productData = new ProductData("tents");
-const productList = new ProductList("tents", productData, document.getElementById("product-list"));
+const category = getParam("category");
+const productData = new ProductData(category);
+const productList = new ProductList(category, productData, document.querySelector(".product-list"));
 
 productList.init();
+
+// const productData = new ProductData("tents");
+// const productList = new ProductList("tents", productData, document.getElementById("product-list"));
+
+// productList.init();
