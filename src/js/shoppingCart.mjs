87638a50f89b082a558f58d79673c8f359c46f87
Giorubio27 <Giorubio27@gmail.com>
2026-04-03 09:36:10 -0600

    I added the calc and display total function to my ShoppingCart Class

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -1,7 +1,7 @@
-import {  getLocalStorage, renderListWithTemplate } from "./utils.mjs";
+import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
 
 function cartItemTemplate(item) {
-      const newItem = `<li class="cart-card divider">
+  const newItem = `<li class="cart-card divider">
       <a href="#" class="cart-card__image">
         <img
           src="${item.Image}"
@@ -16,21 +16,42 @@ function cartItemTemplate(item) {
       <p class="cart-card__price">$${item.FinalPrice}</p>
     </li>`;
 
-      return newItem;
-    }
+  return newItem;
+}
 
 export default class ShoppingCart {
-    constructor(key, listElement) {
-        this.key = key;
-        this.listElement = listElement;
-    }
+  constructor(key, listElement) {
+    this.key = key;
+    this.listElement = listElement;
+  }
 
-    async init() {
-        const list = await getLocalStorage(this.key) || [];
-        this.renderList(list);
-    }
+  async init() {
+    const list = await getLocalStorage(this.key) || [];
+    this.renderList(list);
 
-    renderList(list) {
-        renderListWithTemplate(cartItemTemplate, this.listElement, list);
+    if (list.length > 0) {
+      this.calculateTotal(list);
     }
+  }
+
+  calculateTotal(list) {
+    const total = list.reduce((accumulator, item) => {
+      return accumulator + parseFloat(item.FinalPrice);
+    }, 0);
+
+    this.displayCartTotal(total);
+  }
+
+  displayCartTotal(total) {
+    const cartFooter = document.querySelector(".cart-footer");
+    const cartTotalElement = document.querySelector(".cart-total");
+
+    cartTotalElement.innerText = `Total: $${total.toFixed(2)}`;
+
+    cartFooter.classList.remove("hide");
+  }
+
+  renderList(list) {
+    renderListWithTemplate(cartItemTemplate, this.listElement, list);
+  }
 }
